3a8203f8a378d393820a5ba88c3b6761a1dd720e
tzik <mail@tzik.jp>
2017-05-21 23:58:53 +0900

    Update ninja build

--- pkg/ninja/bin/package.sh
+++ pkg/ninja/bin/package.sh
@@ -6,7 +6,6 @@ toolchain_dir="$(realpath -s "${base_dir}/../..")"
 
 src_dir="${base_dir}/src"
 
-ccache_dir="${base_dir}/ccache"
 image_dir="${base_dir}/image"
 prefix="${toolchain_dir}/out/usr"
 
@@ -14,4 +13,4 @@ package_name="$(basename "${base_dir}")"
 package_file="${base_dir}/${package_name}.tar"
 metadata_file="${prefix}/packages/${package_name}"
 
-mkdir -p "${ccache_dir}" "${image_dir}"
+mkdir -p "${image_dir}"
