85c6ea2c5780bfae0fb81964fc9d9caa549fca1c
tzik <mail@tzik.jp>
2017-05-22 09:21:40 +0900

    Fix rpath-fix

--- lib/pack.sh
+++ lib/pack.sh
@@ -3,15 +3,25 @@ mkdir -p "${image_dir}${prefix}" "$(dirname "${image_dir}${metadata_file}")"
 cd "${image_dir}${prefix}"
 find . -depth -print0 > "${image_dir}${metadata_file}"
 
-if which patchelf > /dev/null 2>&1; then
-  find "${image_dir}${prefix}/bin" -type f -print0 | \
+rpath_fix() {
+  local target="$1"
+  if [ ! -d "${target}" ]; then
+    return 0
+  fi
+
+  find "${target}" -type f -print0 | \
     while read -d $'\0' i; do
       if ! patchelf --print-rpath "$i" > /dev/null 2>&1; then
-         continue
+        continue
       fi
 
       patchelf --set-rpath '$ORIGIN/../lib' "$i"
     done
+}
+
+if which patchelf > /dev/null 2>&1; then
+  rpath_fix "${image_dir}${prefix}/bin"
+  rpath_fix "${image_dir}${prefix}/libexec"
 fi
 
 rm -f "${package_file}"
