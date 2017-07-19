7711c97bf8042ab08c73c6e39696650720fd0fb2
tzik <mail@tzik.jp>
2017-07-19 19:35:15 +0900

    Add z3

--- bin/pkgs.sh
+++ bin/pkgs.sh
@@ -8,4 +8,5 @@ pkgs=(
   "binaryen"
   "wabt"
   "protobuf"
+  "z3"
 )
--- lib/configure-cmake.sh
+++ lib/configure-cmake.sh
@@ -3,8 +3,8 @@ flags+=(
   "-GNinja"
   "-DCMAKE_BUILD_TYPE=Release"
   "-DCMAKE_INSTALL_PREFIX=${prefix}"
+  "-DCMAKE_INSTALL_LIBDIR=lib"
 )
 
 cd "${build_dir}"
 cmake "${flags[@]}" "${cmake_dir}"
-
