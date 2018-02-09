8d72c8b6e1e8f5926280b890c5725daf65c4ce85
tzik <mail@tzik.jp>
2018-02-10 01:07:44 +0900

    Bump

--- env.sh
+++ env.sh
@@ -1,2 +1,5 @@
 toolchain_dir="$(dirname "${BASH_SOURCE:-$0}")"
 path=("${toolchain_dir}/out/usr/bin"(N) "${path[@]}")
+
+# TODO: Build gn by ourselves.
+export CHROMIUM_BUILDTOOLS_PATH="${toolchain_dir}/pkg/v8/v8/buildtools"
